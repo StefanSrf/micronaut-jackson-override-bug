62becefae1dd274a29d071f823b294cd5be74679
Stefan Vettiger <stefan.vettiger@srf.ch>
2024-01-05 08:27:03 +0100

    use BeanCreatedEventListener, which also doesn't work

--- src/main/kotlin/com/example/JacksonObjectMapperFactory.kt
+++ src/main/kotlin/com/example/JacksonObjectMapperFactory.kt
@@ -2,31 +2,16 @@ package com.example
 
 import com.fasterxml.jackson.annotation.JsonSetter
 import com.fasterxml.jackson.annotation.Nulls
-import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.micronaut.context.annotation.Factory
-import io.micronaut.context.annotation.Replaces
-import io.micronaut.jackson.JacksonConfiguration
-import io.micronaut.jackson.ObjectMapperFactory
+import io.micronaut.context.event.BeanCreatedEvent
+import io.micronaut.context.event.BeanCreatedEventListener
 import jakarta.inject.Singleton
 
-// The commented-out JacksonObjectMapperFactory further down works, but it doesn't subclass ObjectMapperFactory which
-// causes other problems, e.g. when trying to push logger config with build-in loggers-endpoint.
-@Factory
-@Replaces(ObjectMapperFactory::class)
-class JacksonObjectMapperFactory: ObjectMapperFactory() {
+@Singleton
+class JacksonObjectMapperFactory : BeanCreatedEventListener<ObjectMapper> {
 
-    override fun jsonFactory(jacksonConfiguration: JacksonConfiguration?): JsonFactory {
-        return super.jsonFactory(jacksonConfiguration)
-    }
-
-    @Replaces(ObjectMapper::class)
-    @Singleton
-    override fun objectMapper(
-        jacksonConfiguration: JacksonConfiguration?,
-        jsonFactory: JsonFactory?,
-    ): ObjectMapper {
-        val mapper = super.objectMapper(jacksonConfiguration, jsonFactory)
+    override fun onCreated(event: BeanCreatedEvent<ObjectMapper>): ObjectMapper {
+        val mapper = event.bean
 
         @Suppress("CommentWrapping")
         mapper.setDefaultSetterInfo(
@@ -39,6 +24,36 @@ class JacksonObjectMapperFactory: ObjectMapperFactory() {
         return mapper
     }
 }
+
+// The commented-out JacksonObjectMapperFactory further down works, but it doesn't subclass ObjectMapperFactory which
+// causes other problems, e.g. when trying to push logger config with build-in loggers-endpoint.
+//@Factory
+//@Replaces(ObjectMapperFactory::class)
+//class JacksonObjectMapperFactory: ObjectMapperFactory() {
+//
+//    override fun jsonFactory(jacksonConfiguration: JacksonConfiguration?): JsonFactory {
+//        return super.jsonFactory(jacksonConfiguration)
+//    }
+//
+//    @Replaces(ObjectMapper::class)
+//    @Singleton
+//    override fun objectMapper(
+//        jacksonConfiguration: JacksonConfiguration?,
+//        jsonFactory: JsonFactory?,
+//    ): ObjectMapper {
+//        val mapper = super.objectMapper(jacksonConfiguration, jsonFactory)
+//
+//        @Suppress("CommentWrapping")
+//        mapper.setDefaultSetterInfo(
+//            JsonSetter.Value.construct(
+//                /* nulls = */ Nulls.DEFAULT,
+//                /* contentNulls = */ Nulls.SKIP
+//            )
+//        )
+//
+//        return mapper
+//    }
+//}
 
 //@Factory
 //@Replaces(ObjectMapperFactory::class)
